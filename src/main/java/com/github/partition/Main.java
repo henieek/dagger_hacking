cbda5f0ca1ed328c711265fe214108e1161942aa
Mateusz Herych <heniek@applicake.com>
2013-05-14 01:35:57 +0200

    purged Main content

--- src/main/java/com/github/partition/Main.java
+++ src/main/java/com/github/partition/Main.java
@@ -3,7 +3,5 @@ package com.github.partition;
 public class Main {
 
   public static void main(String[] args) {
-    SimplestInjection injection = new SimplestInjection();
-    NamedInjection named = new NamedInjection();
   }
 }
