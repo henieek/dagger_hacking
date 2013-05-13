59e27c7e3ebfe2d2b1648b83d955ff33cca30183
Mateusz Herych <heniek@applicake.com>
2013-05-14 01:38:52 +0200

    picking one of multiple interface implementations

---
+++ src/main/java/com/github/partition/MultipleImplementationsInterfaceInjection.java
@@ -0,0 +1,28 @@
+package com.github.partition;
+
+import com.github.partition.common.Greeter;
+import com.github.partition.common.HappyGreeter;
+import dagger.Module;
+import dagger.ObjectGraph;
+import dagger.Provides;
+
+import javax.inject.Inject;
+
+public class MultipleImplementationsInterfaceInjection {
+
+  @Inject
+  Greeter iAmHappy;
+
+  public MultipleImplementationsInterfaceInjection() {
+    ObjectGraph.create(HappyModule.class).inject(this);
+  }
+
+  @Module(injects = MultipleImplementationsInterfaceInjection.class)
+  static class HappyModule {
+
+    @Provides
+    public Greeter provideHappyGreeter() {
+      return new HappyGreeter();
+    }
+  }
+}
