a380237a74b5a68e037cfd9007faba7c967322c9
Mateusz Herych <mateusz.herych@getbase.com>
2013-10-26 08:36:41 +0200

    aaa

--- src/main/java/com/github/partition/annotation/Activity.java
+++ src/main/java/com/github/partition/annotation/Activity.java
@@ -1,7 +1,12 @@
 package com.github.partition.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import javax.inject.Qualifier;
 
 @Qualifier
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Activity {
+
 }
